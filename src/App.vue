1bb52844a709c9c471edc24ed7063afd74693477
PHenryy <qwerty.code24@gmail.com>
2019-07-16 17:39:54 +0800

    基本配置

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app" class="">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "app",
+  components: {
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {},
+    colors: {
+      gray: '#F8F8F8',
+      green: '#40F99B',
+      red: '#F15152',
+      'write-primary': '#2F2F2F',
+      'write-secondary': '#666666'
+    }
+  },
+  variants: {},
+  plugins: []
+}
